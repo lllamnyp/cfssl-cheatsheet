1fa2bb7b1851a9af6dd731ab8b83a94425180f22
Timofey Larkin <lllamnyp@gmail.com>
2021-05-25 10:53:36 +0300

    Fixes and more functionality in script

--- gencsr.sh
+++ gencsr.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
+export NAMES_CN=${NAMES_CN:-company.ru}
 export NAMES_C=${NAMES_C:-RU}
 export NAMES_L=${NAMES_L:-Moscow}
 export NAMES_O=${NAMES_O:-Company}
+export NAMES_OU=${NAMES_OU:-WWW}
+export NAMES_ST=${NAMES_ST:-Moscow}
 export EXPIRY=${EXPIRY:-8760h}
+export wildcard=${wildcard:-true}
+
+if $wildcard
+then
+	export hosts="\"$1\",\"*.$1\""
+else
+	export hosts="\"$1\""
+fi
 
 if which cfssl
 then 
@@ -27,14 +38,14 @@ else
 	fi
 fi
 
-chmod a+x cfssl cfssljson
+chmod a+x cfssl cfssljson || true
 
 cat <<EOF > cfssl-cfg.json
 {
   "signing": {
     "default": {
       "expiry": "${EXPIRY}",
-      "usages": ["signing", "key encipherment", "server auth"],
+      "usages": ["signing", "key encipherment", "server auth"]
     }
   }
 }
@@ -42,10 +53,8 @@ EOF
 
 cat <<EOF > csr.json
 {
-    "hosts": [
-        "$1",
-        "*.$1"
-    ],
+    "CN": "${NAMES_CN}",
+    "hosts": [$hosts],
     "key": {
         "algo": "rsa",
         "size": 4096
@@ -54,13 +63,15 @@ cat <<EOF > csr.json
         {
             "C": "${NAMES_C}",
             "L": "${NAMES_L}",
-            "O": "${NAMES_O}"
+            "O": "${NAMES_O}",
+            "OU": "${NAMES_OU}",
+            "ST": "${NAMES_ST}"
         }
     ]
 }
 EOF
 
-${cfssl_binary} genkey csr.json | ${cfssljson_binary} -bare $1
+${cfssl_binary} genkey -config cfssl-cfg.json csr.json | ${cfssljson_binary} -bare $1
 
 echo
 echo Certificate signing requested generated
